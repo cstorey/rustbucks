15f8a8c728e31904f471ef64c220e542c6ddbbc6
Ceri Storey <cez@necrofish.org.uk>
2019-04-19 13:59:12 +0100

    Configure pg builder settings.

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -26,6 +26,7 @@ extern crate tokio_threadpool;
 extern crate maplit;
 
 use std::sync::Arc;
+use std::time::Duration;
 
 use actix_web::server::{HttpHandler, HttpHandlerTask};
 use actix_web::App;
@@ -54,6 +55,11 @@ pub struct Config {
 #[derive(Deserialize, Serialize, Debug, Default)]
 pub struct PgConfig {
     pub url: String,
+    max_size: Option<u32>,
+    min_idle: Option<u32>,
+    max_lifetime: Option<Duration>,
+    idle_timeout: Option<Duration>,
+    connection_timeout: Option<Duration>,
 }
 
 #[derive(Clone)]
@@ -64,27 +70,16 @@ pub struct RustBucks {
 
 impl RustBucks {
     pub fn new(config: &Config) -> Result<Self, Error> {
-        let db = Self::pool(&config.postgres)?;
+        let db = config.postgres.build()?;
+
+        debug!("Init schema");
+        db.get()?.setup().context("Setup persistence")?;
+
         let threads = Arc::new(ThreadPool::new());
         let menu = menu::Menu::new(db.clone(), threads.clone())?;
         let orders = orders::Orders::new(db.clone(), threads.clone())?;
+
         Ok(RustBucks { menu, orders })
-    }
-
-    fn pool(config: &PgConfig) -> Result<Pool<persistence::DocumentConnectionManager>, Error> {
-        debug!("Build pool");
-        let manager = persistence::DocumentConnectionManager::new(
-            PostgresConnectionManager::new(&*config.url, TlsMode::None)
-                .context("connection manager")?,
-        );
-        let builder = r2d2::Pool::builder();
-        debug!("Pool builder: {:?}", builder);
-        let pool = builder.build(manager).context("build pool")?;
-
-        debug!("Init schema");
-        pool.get()?.setup().context("Setup persistence")?;
-
-        Ok(pool)
     }
 
     pub fn app(&self) -> Vec<Box<dyn HttpHandler<Task = Box<dyn HttpHandlerTask>>>> {
@@ -92,5 +87,39 @@ impl RustBucks {
 
         let redir_root = App::new().resource("/", |r| r.get().f(menu::Menu::index_redirect));
         vec![self.menu.app(), self.orders.app(), redir_root.boxed()]
+    }
+}
+
+impl PgConfig {
+    fn build(&self) -> Result<Pool<persistence::DocumentConnectionManager>, Error> {
+        debug!("Build pool from {:?}", self);
+
+        let manager = persistence::DocumentConnectionManager::new(
+            PostgresConnectionManager::new(&*self.url, TlsMode::None)
+                .context("connection manager")?,
+        );
+
+        let mut builder = r2d2::Pool::builder();
+
+        if let Some(max_size) = self.max_size {
+            builder = builder.max_size(max_size);
+        }
+        if let Some(min_idle) = self.min_idle {
+            builder = builder.min_idle(Some(min_idle));
+        }
+        if let Some(max_lifetime) = self.max_lifetime {
+            builder = builder.max_lifetime(Some(max_lifetime));
+        }
+        if let Some(idle_timeout) = self.idle_timeout {
+            builder = builder.idle_timeout(Some(idle_timeout));
+        }
+        if let Some(connection_timeout) = self.connection_timeout {
+            builder = builder.connection_timeout(connection_timeout);
+        }
+
+        debug!("Pool builder: {:?}", builder);
+        let pool = builder.build(manager).context("build pool")?;
+
+        Ok(pool)
     }
 }
