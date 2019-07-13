234267a8fb50bf3569bf86b2169bd41093c6e2d5
Ceri Storey <cez@necrofish.org.uk>
2019-07-13 22:29:03 +0100

    Add orders page.

--- temple_plates/__init__.py
+++ temple_plates/__init__.py
@@ -8,6 +8,7 @@ def main(global_config, **settings):
     config.include('pyramid_jinja2')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('drink', '/menu/{id}')
+    config.add_route('new_order', '/orders')
     config.add_route('menu', '/')
     config.scan()
     return config.make_wsgi_app()
--- temple_plates/views.py
+++ temple_plates/views.py
@@ -9,3 +9,10 @@ def menu(request):
 def drink(request):
     drink_id=request.matchdict['id']
     return dict(drink_id=drink_id)
+
+@view_config(route_name='new_order', renderer='templates/order.jinja2')
+def new_order(request):
+    print(('matches', request.matchdict))
+    print(('params', request.params))
+    drink_id=request.params['drink_id']
+    return dict(drink_id=drink_id)
