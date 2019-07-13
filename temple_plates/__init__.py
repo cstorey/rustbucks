d90f91caa1a2db3782b36fbdb851b8741a05f03b
Ceri Storey <cez@necrofish.org.uk>
2019-07-13 22:21:20 +0100

    Add drink template.

--- temple_plates/__init__.py
+++ temple_plates/__init__.py
@@ -7,6 +7,7 @@ def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
     config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_route('drink', '/menu/{id}')
     config.add_route('menu', '/')
     config.scan()
     return config.make_wsgi_app()
--- temple_plates/views.py
+++ temple_plates/views.py
@@ -3,4 +3,9 @@ from pyramid.view import view_config
 
 @view_config(route_name='menu', renderer='templates/menu.jinja2')
 def menu(request):
-    return {'project': 'temple-plates'}
+    return {}
+
+@view_config(route_name='drink', renderer='templates/drink.jinja2')
+def drink(request):
+    drink_id=request.matchdict['id']
+    return dict(drink_id=drink_id)
