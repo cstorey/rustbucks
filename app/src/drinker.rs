dfeeb41be46ed30970651937ce0dc0c6fe8ade4c
Ceri Storey <cez@necrofish.org.uk>
2019-08-19 20:59:25 +0100

    Note how we show we can buy two teas.

--- app/src/drinker.rs
+++ app/src/drinker.rs
@@ -10,7 +10,7 @@ use infra::ids::{Entity, Id, IdGen};
 pub struct Drinker {
     #[serde(flatten)]
     pub(crate) meta: DocMeta<Drinker>,
-    received_drinks: HashSet<Id<menu::Drink>>,
+    pub(crate) received_drinks: HashSet<Id<menu::Drink>>,
 }
 
 impl Drinker {
@@ -22,16 +22,6 @@ impl Drinker {
             meta,
             received_drinks,
         }
-    }
-
-    #[cfg(test)]
-    pub fn has_drink(&self, drink_id: Id<menu::Drink>) -> bool {
-        self.received_drinks.contains(&drink_id)
-    }
-
-    #[cfg(test)]
-    pub fn deliver_drink(&mut self, drink_id: Id<menu::Drink>) {
-        self.received_drinks.insert(drink_id);
     }
 }
 
--- app/src/test/mod.rs
+++ app/src/test/mod.rs
@@ -4,7 +4,7 @@ use env_logger;
 use failure::Fallible;
 
 use crate::drinker::Drinker;
-use crate::ids::IdGen;
+use crate::ids::{Id, IdGen};
 use crate::menu::Drink;
 use crate::orders::Order;
 
@@ -34,15 +34,43 @@ fn trivial_order_workflow_as_transaction_script() -> Fallible<()> {
         drinker,
         tea
     );
-    #[cfg(never)]
-    {}
     Ok(())
+}
+
+#[cfg(test)]
+impl Drinker {
+    fn has_drink(&self, drink_id: Id<Drink>) -> bool {
+        self.received_drinks.contains(&drink_id)
+    }
+
+    fn deliver_drink(&mut self, drink_id: Id<Drink>) {
+        self.received_drinks.insert(drink_id);
+    }
 }
 
 #[test]
 #[ignore]
 fn trivial_order_workflow_for_two_teas() -> Fallible<()> {
-    unimplemented!("trivial_order_workflow_when_out_of_milk")
+    env_logger::try_init().unwrap_or_default();
+    let pool = crate::persistence::test::pool("trivial_order_workflow_for_two_teas")?;
+    let conn = pool.get()?;
+    let idgen = IdGen::new();
+    let mut tea = Drink::new(idgen.generate(), "bubble tea");
+    conn.save(&mut tea)?;
+
+    let mut drinker = Drinker::incarnate(&idgen);
+    conn.save(&mut drinker)?;
+
+    let mut order = Order::for_drink(tea.meta.id, drinker.meta.id, &idgen);
+    conn.save(&mut order)?;
+    drinker.deliver_drink(order.drink_id);
+    conn.save(&mut drinker)?;
+
+    drinker.deliver_drink(order.drink_id);
+    conn.save(&mut drinker)?;
+
+    assert_eq!(drinker.received_drinks.len(), 2);
+    Ok(())
 }
 
 #[test]
